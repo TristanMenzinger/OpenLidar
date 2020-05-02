242b7da955f8c58e9ffa9b41846d66d4c2db4b19
Tristan Menzinger <t.menzinger@gmail.com>
2020-05-02 19:31:49 +0200

    added build script, added contact method

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
-uglifyjs js/OrbitControls.js > js_min/OrbitControls.js
-uglifyjs js/pako.js > js_min/pako.js
-uglifyjs js/proj4.js > js_min/proj4.js
-uglifyjs js/three.js > js_min/three.js
-uglifyjs js/WebGL.js > js_min/WebGL.js
+mkdir -p js
+mkdir -p css
+
+echo "Minifying Javascript"
+uglifyjs source/js/OrbitControls.js > js/OrbitControls.js
+uglifyjs source/js/pako.js > js/pako.js
+uglifyjs source/js/proj4.js > js/proj4.js
+uglifyjs source/js/three.js > js/three.js
+uglifyjs source/js/WebGL.js > js/WebGL.js
+
+echo "Creating language files"
+./source/languages/language_fill.sh source/html/index.html de/index.html source/languages/de.lang
+./source/languages/language_fill.sh source/html/index.html en/index.html source/languages/en.lang
+
+./source/languages/language_fill.sh source/html/email.html de/email.html source/languages/de.lang
+./source/languages/language_fill.sh source/html/email.html en/email.html source/languages/en.lang
+
+echo "Minifying css"
+uglifycss source/css/maps_override.css > css/maps_override.css
+uglifycss source/css/main.css > css/main.css
+
+echo "Done"
+
---
+++ source/languages/language_fill.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Check if arguments were supplied
+if (( $# < 3 )); then
+    echo "Three arguments required: path/to/input/file.html path/to/output/file.html and /path/to/language/file.lang"
+    exit 1
+fi
+
+htmli="$1"
+htmlo="$2"
+input="$3"
+
+languageline=$(head -n 1 $input)
+
+if ! [[ "$languageline" == LANGUAGE* ]]; then 
+	# Terminate
+	echo "Language missing"
+	exit 1
+fi
+
+if ! [[ $(tail -c1 "$input" | wc -l) -gt 0 ]]; then
+	echo "File does not end on newline. Please add final newline to $input."
+fi
+
+
+values=(${languageline//=/ })
+language=$(echo "${values[1]}" | tr '[:upper:]' '[:lower:]')
+
+echo "Converting language: $language from $htmli to $htmlo"
+mkdir -p "$language"
+cp "$htmli" "$htmlo"
+
+first=true
+while read -r line; do
+
+	# Skip first line
+	if [[ "$first" = true ]]; then
+		first=false
+		continue
+	fi
+
+	# Check if empty
+	linet="$(echo -e "${line}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+	if [[ "$linet" == "" ]]; then
+		continue
+	fi
+
+	# Check if uncommented
+	if [[ "$linet" =~ ^//.* ]]; then
+    	continue
+	fi
+
+	# Key-Value split
+	values=(${linet//=/ })
+	key="${values[0]}"
+	value="${values[1]}"
+	# echo "$key - $value"
+
+	# On linux, this line will fail als sed doesn't need the ''
+	sed -i '' "s|{{$key}}|$value|g" "$htmlo"
+
+
+done < "$input"
